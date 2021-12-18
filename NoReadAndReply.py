789a904c1d25aeb9c2aaf6e2993119784ea7a913
YuamLu <88614618+YuamLu@users.noreply.github.com>
2021-12-19 00:15:36 +0800

    打包成腳本，運作後自動複製到剪貼簿

---
+++ NoReadAndReply.py
@@ -0,0 +1,38 @@
+'''
+/NoReadAndReply.ipynb
+by_YuAN on 12/15/2021
+'''
+import pygame,time,win32clipboard as clip
+import win32con
+# ------顏色------
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+# ------交互------
+text = "今天過得如何?"
+# ----------------
+
+if __name__ == '__main__':
+    print('#----------------#')
+    print('# NoReadAndReply #')
+    print('#----------------#')
+    pygame.init()
+    font = pygame.font.SysFont('Microsoft YaHei', 100)    
+
+    print('請按提示輸入文字並設定相關參數，此程式會生成圖片檔並複製到剪貼簿中。')
+
+    print('請輸入文字: ',end='');text = input();print(text)
+    print('聊天室是否為黑色背景?(y/N): ',end='');mode = input();print(mode)
+    if mode == 'y':ftext = font.render(text, True, WHITE, BLACK)
+    else:ftext = font.render(text, True, BLACK, WHITE)
+
+    pygame.image.save(ftext, "image.bmp")
+    clip.OpenClipboard()
+    clip.EmptyClipboard()
+    with open("image.bmp", "rb") as f: data = f.read()[14:]
+    clip.SetClipboardData(win32con.CF_DIB, data)
+    clip.CloseClipboard()
+
+    
+
+
+
